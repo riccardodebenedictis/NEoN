fdd9f6fe8497c1a6bad740cde5000c6a56f572f7
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2018-05-24 17:50:16 +0200

    restored gui and tests..

---
+++ nn/tests/test0.cpp
@@ -0,0 +1,32 @@
+#include "network.h"
+#include <cassert>
+
+using namespace nn;
+
+int main(int argc, char *argv[])
+{
+    // we create the network..
+    mean_squared_error mse;
+    sigmoid sgm;
+    network net(mse, sgm, {2, 3, 1});
+
+    // we create the training data..
+    std::vector<data_row *> tr_data({new data_row({3, 5}, {0.75}),
+                                       new data_row({5, 1}, {0.82}),
+                                       new data_row({10, 2}, {0.93})});
+                                       
+    // we create the test data..
+    std::vector<data_row *> tst_data({new data_row({3, 5}, {0.75}),
+                                       new data_row({5, 1}, {0.82}),
+                                       new data_row({10, 2}, {0.93})});
+
+    // this is the current error on training data before the training..
+    double c_err = net.get_error(tr_data);
+
+    // we train the network through stochastic gradient descent..
+    net.sgd(tr_data, tst_data, 200, 3, 3, 0.1);
+
+    // this is the current error on training data after the training..
+    double t_err = net.get_error(tr_data);
+    assert(c_err > t_err);
+}
---
+++ nn/tests/test1.cpp
@@ -0,0 +1,34 @@
+#include "network.h"
+#include <cassert>
+
+using namespace nn;
+
+int main(int argc, char *argv[])
+{
+    // we create the network..
+    cross_entropy ce;
+    sigmoid sgm;
+    network net(ce, sgm, {2, 4, 4, 1});
+
+    // we create the training data..
+    std::vector<data_row *> tr_data({new data_row({0, 0}, {0}),
+                                       new data_row({0, 1}, {1}),
+                                       new data_row({1, 0}, {1}),
+                                       new data_row({1, 1}, {0})});
+
+    // we create the test data..
+    std::vector<data_row *> tst_data({new data_row({0, 0}, {0}),
+                                       new data_row({0, 1}, {1}),
+                                       new data_row({1, 0}, {1}),
+                                       new data_row({1, 1}, {0})});
+
+    // this is the current error on training data before the training..
+    double c_err = net.get_error(tr_data);
+
+    // we train the network through stochastic gradient descent..
+    net.sgd(tr_data, tst_data, 2000, 2, 0.05, 0.1);
+
+    // this is the current error on training data after the training..
+    double t_err = net.get_error(tr_data);
+    assert(c_err > t_err);
+}
